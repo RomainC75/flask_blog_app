5bac8addd0a3075652158ea044285ab6e6f73f43
RomainC75 <rom.chenard@gmail.com>
2022-11-28 12:08:03 +0100

    psycog2-library

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from flask_gravatar import Gravatar
 from forms import CreatePostForm, UserRegisterForm, UserLoginForm, CommentForm
 from datetime import datetime
 from utils import admin_only, row2dict
-
+import os
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -24,7 +24,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
