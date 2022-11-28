73820957c0c3a3fbfa10e011078e90d7d6e797d6
RomainC75 <rom.chenard@gmail.com>
2022-11-28 12:15:43 +0100

    ...

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
